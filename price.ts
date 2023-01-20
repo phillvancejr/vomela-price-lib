c9716906fe76e8b9864442849fa45e3553be6fc0
phillvancejr <phillipvancejr@gmail.com>
2023-01-20 16:23:29 -0500

    price

---
+++ price.ts
@@ -0,0 +1,36 @@
+interface QuotePair {
+    print: number;
+    plain: number;
+}
+
+interface Price {
+    sf: number;
+    price: number;
+}
+
+interface Total extends Price {
+    print: Price;
+    plain: Price;
+}
+
+function price(sf: number, cost: number): Price {
+    cost *= sf;
+    return { sf, price: cost };
+}
+export function get_total(cost: QuotePair, sf: QuotePair): Total {
+    const { print: print_cost, plain: plain_cost } = cost;
+    const { print: print_sf, plain: plain_sf } = sf;
+
+    const print = price(print_sf,print_cost);
+    const plain = price(plain_sf,plain_cost);
+
+    const total = {
+        sf: print.sf + plain.sf,
+        price: print.price + plain.price,
+        print,
+        plain, 
+    };
+
+    return total;
+}
+
